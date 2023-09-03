7b9d5771e3eb778a99b9e73cc61e34502537d9ae
winstxnhdw <winstxnhdw@gmail.com>
2023-09-04 04:01:20 +0800

    feat: use OpenAPI router

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,23 @@
+import { OpenAPIRouter } from '@cloudflare/itty-router-openapi'
+
 async function main(request: Request): Promise<Response> {
   return new Promise((resolve) => resolve(new Response(request.body, { status: 200 })))
 }
 
+const router = OpenAPIRouter({
+  docs_url: '/docs',
+  schema: {
+    info: {
+      title: 'worker',
+      description: 'A worker',
+      version: 'v1.0.0'
+    }
+  }
+})
+
+router.get('/', main)
+router.all('*', () => new Response('Not found!', { status: 404 }))
+
 export default {
-  fetch: main
+  fetch: router.handle
 }
