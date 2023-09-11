96a9de3cef93b715b8a7c067b97350c6ea519e21
winstxnhdw <winstxnhdw@gmail.com>
2023-09-11 21:40:28 +0800

    feat: add CORS support

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { OpenAPIRouter } from '@cloudflare/itty-router-openapi'
+import { createCors } from 'itty-router'
 
 async function main(request: Request): Promise<Response> {
   return new Promise((resolve) => resolve(new Response(request.body, { status: 200 })))
@@ -15,9 +16,16 @@ const router = OpenAPIRouter({
   }
 })
 
+const { preflight, corsify } = createCors({
+  methods: ['*'],
+  origins: ['*']
+})
+
+router.all('*', preflight)
 router.get('/', main)
 router.all('*', () => new Response('Not found!', { status: 404 }))
 
 export default {
-  fetch: router.handle
+  fetch: async (request: Request, env: unknown, ctx: unknown) =>
+    router.handle(request, env, ctx).catch(console.error).then(corsify)
 }
