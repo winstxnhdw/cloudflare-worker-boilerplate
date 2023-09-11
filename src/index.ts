d66aaeca5bbc2d07ccc26a46c889d9f12ffdf92b
winstxnhdw <winstxnhdw@gmail.com>
2023-09-12 03:10:44 +0800

    refactor: avoid global state

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,35 @@
+import type { Environment } from '@/types'
 import { OpenAPIRouter } from '@cloudflare/itty-router-openapi'
 import { createCors } from 'itty-router'
 
-async function main(request: Request): Promise<Response> {
+async function controller(request: Request): Promise<Response> {
   return new Promise((resolve) => resolve(new Response(request.body, { status: 200 })))
 }
 
-const router = OpenAPIRouter({
-  docs_url: '/docs',
-  schema: {
-    info: {
-      title: 'worker',
-      description: 'A worker',
-      version: 'v1.0.0'
+async function main(request: Request, env: Environment, ctx: unknown) {
+  const router = OpenAPIRouter({
+    docs_url: '/docs',
+    schema: {
+      info: {
+        title: 'worker',
+        description: 'A worker',
+        version: 'v1.0.0'
+      }
     }
-  }
-})
+  })
 
-const { preflight, corsify } = createCors({
-  methods: ['*'],
-  origins: ['*']
-})
+  const { preflight, corsify } = createCors({
+    methods: ['*'],
+    origins: ['*']
+  })
 
-router.all('*', preflight)
-router.get('/', main)
-router.all('*', () => new Response('Not found!', { status: 404 }))
+  router.all('*', preflight)
+  router.get('/', controller)
+  router.all('*', () => new Response('Not found!', { status: 404 }))
+
+  return router.handle(request, env, ctx).catch(console.error).then(corsify)
+}
 
 export default {
-  fetch: async (request: Request, env: unknown, ctx: unknown) =>
-    router.handle(request, env, ctx).catch(console.error).then(corsify)
+  fetch: main
 }
---
+++ src/types/index.ts
@@ -0,0 +1,1 @@
+export { Environment } from '@/types/environment'
