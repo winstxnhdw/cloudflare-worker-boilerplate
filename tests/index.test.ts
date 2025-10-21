b22f271e11ba83e525c4ba82974517bf5a9cf514
winstxnhdw <winstxnhdw@gmail.com>
2025-10-21 14:12:52 +0800

    style: use camelCase

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -2,12 +2,12 @@ import { describe } from 'bun:test';
 import { treaty } from '@elysiajs/eden';
 import { app } from '@/app';
 
-function create_app() {
+function createApp() {
   const api = app({});
 
   return treaty(api);
 }
 
 describe('worker', () => {
-  const api = create_app();
+  const api = createApp();
 });
