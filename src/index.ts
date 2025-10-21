e5570943bb7d8e35db0a6cc1300b57da46d954e6
winstxnhdw <winstxnhdw@gmail.com>
2025-10-22 05:01:38 +0800

    chore: cast `env` as `Bindings`

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import { env } from 'cloudflare:workers';
 import { Elysia } from 'elysia';
 import { CloudflareAdapter } from 'elysia/adapter/cloudflare-worker';
 import { app } from '@/app';
+import type { Bindings } from '@/types';
 
-export default new Elysia({ adapter: CloudflareAdapter }).use(app(env)).compile();
+export default new Elysia({ adapter: CloudflareAdapter }).use(app(env as Bindings)).compile();
